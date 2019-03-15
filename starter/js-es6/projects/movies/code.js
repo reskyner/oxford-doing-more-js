01d731ae275e24a98b4909df4a7d38ced9ddf1fa
John Coumbe <john.coumbe@gmail.com>
2019-03-15 22:09:32 +0000

    PROJECTS folder

---
+++ starter/js-es6/projects/movies/code.js
@@ -0,0 +1,8 @@
+
+let films = [
+	{ title:"Raging Bull", image:"ragingbull.jpg", id:"rb"},
+	{ title:"GoodFellas", image:"goodfellas.jpg", id:"gf"},
+	{ title:"Silence", image:"silence.jpg",id:"si"},
+	{ title:"Taxi Driver", image:"taxidriver.jpg",id:"td"},
+	{ title:"The Departed", image:"thedeparted.jpg",id:"dp"}
+]
